c9043db506b1291e6ad98fb095cd7d99fb4db655
Mohammad Rezazadeh <mohammad1382144@gmail.com>
2024-11-16 23:51:54 +0330

    Polymorphism

---
+++ tamrin6/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace tamrin6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCar = new System.Windows.Forms.Button();
+            this.ButtonBike = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonCar
+            // 
+            this.ButtonCar.Location = new System.Drawing.Point(12, 12);
+            this.ButtonCar.Name = "ButtonCar";
+            this.ButtonCar.Size = new System.Drawing.Size(309, 114);
+            this.ButtonCar.TabIndex = 0;
+            this.ButtonCar.Text = "Car info";
+            this.ButtonCar.UseVisualStyleBackColor = true;
+            this.ButtonCar.Click += new System.EventHandler(this.ButtonCar_Click);
+            // 
+            // ButtonBike
+            // 
+            this.ButtonBike.Location = new System.Drawing.Point(12, 141);
+            this.ButtonBike.Name = "ButtonBike";
+            this.ButtonBike.Size = new System.Drawing.Size(309, 114);
+            this.ButtonBike.TabIndex = 1;
+            this.ButtonBike.Text = "Bike info";
+            this.ButtonBike.UseVisualStyleBackColor = true;
+            this.ButtonBike.Click += new System.EventHandler(this.ButtonBike_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 278);
+            this.Controls.Add(this.ButtonBike);
+            this.Controls.Add(this.ButtonCar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCar;
+        private System.Windows.Forms.Button ButtonBike;
+    }
+}
+
---
+++ tamrin6/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tamrin6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCar_Click(object sender, EventArgs e)
+        {
+            vehicle car = new Car();
+            car.start();
+        }
+
+        private void ButtonBike_Click(object sender, EventArgs e)
+        {
+            vehicle bake = new Bake();
+            bake.start();
+        }
+    }
+    public abstract class vehicle
+    {
+        public abstract void start();
+    }
+    public class Car : vehicle
+    {
+        public override void start()
+        {
+            MessageBox.Show("After we start, you can enjoy your journey with peace of mind");
+        }
+    }
+        public class Bake : vehicle
+        {
+            public override void start()
+            {
+                MessageBox.Show("After starting with the bike, your legs will start to get tired");
+            }
+        }
+    }
+
